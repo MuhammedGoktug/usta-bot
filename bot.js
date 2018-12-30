76bd2b08f11417f8f185341b42297b1ad5d5d144
AtakanUsta <46166085+AtakanUsta@users.noreply.github.com>
2018-12-31 00:35:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,18 +97,6 @@ client.on('message', msg => {
         }})
     }
 	
-bot.on('guildMemberRemove', member => {
-  const channel = member.guild.channels.find('name', 'gelen-giden');// 'gelen-giden' log ismidir. değiştirebilirsiniz. belirttiğiniz isme giriş çıkış gösterecektir.
-  if (!channel) return;
-  const embed = new Discord.RichEmbed()
-  .setColor('0xff1a1a')
-  .setAuthor(bot.user.username, bot.user.avatarURL)
-  .setThumbnail(member.user.avatarURL)
-  .setTitle(`:outbox_tray: ${member.user.username} Sunucudan ayrıldı.`)
-  .setTimestamp()
-  channel.sendEmbed(embed);
-});	
-	
 	
 });
 
